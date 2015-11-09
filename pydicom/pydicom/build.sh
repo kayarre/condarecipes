843c807071633aafad746a7e2c36fbcd4aebb8da
Kurt Sansom <kayarre@gmail.com>
2015-11-08 23:40:13 -0800

    updated pydicom to current from origin

---
+++ pydicom/pydicom/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+$PYTHON setup.py install
+$PYTHON setup.py test
+
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
